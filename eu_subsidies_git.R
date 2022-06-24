4dd237f429e07fa2bb0201aa81115421396570be
PhilippAusgust <s7phmuen@uni-bonn.de>
2022-06-24 23:57:15 +0200

    Initial commit

---
+++ eu_subsidies_git.R
@@ -0,0 +1,21 @@
+library(decisionSupport)
+
+DiagrammeR("
+   graph LR
+     WaterProtection-->Circulation
+     ShutDownAreas-->Circulation
+     LessPesticides-->Circulation
+     Intercropping-->Circulation
+     FloweringStrips-->Circulation
+     Circulation -->EUSubsidies
+     Circulation -->Yield
+     Circulation -->Effort
+     Effort -->Income
+     Yield -->Income
+     EUSubsidies -->Income
+     ConventionalAgriculture -->EUSubsidies
+     ConventionalAgriculture -->Yield
+     Income -->FinalDecision
+     Water_Protection-->ConventionalAgriculture
+     Fertilizer_maximum-->ConventionalAgriculture
+ ")
