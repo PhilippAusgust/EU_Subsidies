e8cc6561d70d212adf69503fa16fd65858d5ee54
PhilippAusgust <s7phmuen@uni-bonn.de>
2022-07-13 12:23:21 +0200

    new Code for Diagramm added

--- Diagramm.R
+++ Diagramm.R
@@ -1,67 +1,59 @@
 library(decisionSupport)
 library(DiagrammeR)
+#help(DiagrammeR)
+
+#Previous Diagram ####
+
+# DiagrammeR("
+#    graph LR
+#      WaterProtection-->Circulation
+#      ShutDownAreas-->Circulation
+#      LessPesticides-->Circulation
+#      Intercropping-->Circulation
+#      FloweringStrips-->Circulation
+#      Circulation -->EUSubsidies
+#      Circulation -->Yield
+#      Circulation -->Effort
+#      Effort -->Income
+#      Yield -->Income
+#      EUSubsidies -->Income
+#      ConventionalAgriculture -->EUSubsidies
+#      ConventionalAgriculture -->Yield
+#      Income -->FinalDecision
+#      Water_Protection-->ConventionalAgriculture
+#      Fertilizer_maximum-->ConventionalAgriculture
+#  ")
+# 
 
 
-
-DiagrammeR("
-   graph LR
-     WaterProtection-->Circulation
-     ShutDownAreas-->Circulation
-     LessPesticides-->Circulation
-     Intercropping-->Circulation
-     FloweringStrips-->Circulation
-     Circulation -->EUSubsidies
-     Circulation -->Yield
-     Circulation -->Effort
-     Effort -->Income
-     Yield -->Income
-     EUSubsidies -->Income
-     ConventionalAgriculture -->EUSubsidies
-     ConventionalAgriculture -->Yield
-     Income -->FinalDecision
-     Water_Protection-->ConventionalAgriculture
-     Fertilizer_maximum-->ConventionalAgriculture
- ")
-
-
-
-help(DiagrammeR)
+#Final Diagram ####
 
 DiagrammeR("
            
            graph LR
-           
            I(income)-->A(final_decision)
+
            S(Subsidies) -->I
-           YY(TRUE)-->S
-           BB(TRUE)-->S
-           MM(TRUE)-->S
+           BB(basic_subsidies)-->S
            
-           Y(young_farmers)-->YY
-           Y-->aa(FALSE)
-           B(basic_subsidies)-->BB
-           B--> aaa(FALSE)
+           MM(small_fields_subsidies)-->S
+           B(basic_subsidies_costs)-->BB
            
-           M(max_field_size)-->MM
-           M-->aaaa(FALSE)
-           a(age_farmer)-->Y
-           q(qualification_farmer)-->Y
-           qq(max_support_time)-->Y
-           r(4%_shutdown)-->B
+           r(4%_costs_shutdown)-->B
            k(conversation_tillage)-->B
-           ww(water_protection)--> B
-           pgl(permanent_grass_land)-->B
+           ww(costs_water_protection)--> B
+           pgl(costs_cns_tillage)-->B
            
-           ef(each_field<5 ha)-->M
+           M(costs_field_area_<_5_ha)-->MM
+           ef(more_cultivation_costs)-->M
            
            
            p(market_price)-->I
-           
            YI(Yield)-->I
-           ppt(plant_protection)-->YI
-           fez(fertilizer)-->YI
-           cir(circulation_to_get_subsidies)-->YI
-           w(weather_conditions)--> YI
+           
+           
+           
+           
            
            c(costs)-->I
            
@@ -70,40 +62,18 @@ DiagrammeR("
            cm(costs_machinery)-->c
            hi(hail_insurance)-->c
            
-           
-           
-           
-           
-            
-           
-           
-           
-           
-           
+
            
            style S fill:lightgreen, stroke:#333,stroke-width:2px;
-           style a fill:lightgreen, stroke:#333,stroke-width:2px;
-           style q fill:lightgreen, stroke:#333,stroke-width:2px;
-           style qq fill:lightgreen, stroke:#333,stroke-width:2px;
-           style r fill:lightgreen, stroke:#333,stroke-width:2px;
-           style k fill:lightgreen, stroke:#333,stroke-width:2px;
-           style ww fill:lightgreen, stroke:#333,stroke-width:2px;
-           style pgl fill:lightgreen, stroke:#333,stroke-width:2px;
-           style ef fill:lightgreen, stroke:#333,stroke-width:2px;
-           style M fill:lightgreen, stroke:#333,stroke-width:2px;
-           style B fill:lightgreen, stroke:#333,stroke-width:2px;
-           style Y fill:lightgreen, stroke:#333,stroke-width:2px;
-           
-           style YY fill:darkgreen, stroke:#333,stroke-width:2px;
-           style BB fill:darkgreen, stroke:#333,stroke-width:2px;
-           style MM fill:darkgreen, stroke:#333,stroke-width:2px;
-           
-           
-           style aa   fill:pink, stroke:#333,stroke-width:2px;
-           style aaa  fill:pink, stroke:#333,stroke-width:2px;
-           style aaaa fill:pink, stroke:#333,stroke-width:2px;
-           
-           
+           style r fill:orange, stroke:#333,stroke-width:2px;
+           style k fill:orange, stroke:#333,stroke-width:2px;
+           style ww fill:orange, stroke:#333,stroke-width:2px;
+           style pgl fill:orange, stroke:#333,stroke-width:2px;
+           style ef fill:orange, stroke:#333,stroke-width:2px;
+           style M fill:pink, stroke:#333,stroke-width:2px;
+           style B fill:pink, stroke:#333,stroke-width:2px;
+           style BB fill:lightgrey, stroke:#333,stroke-width:2px;
+           style MM fill:lightgrey, stroke:#333,stroke-width:2px;
            style saf   fill:red, stroke:#333,stroke-width:2px;
            style cp    fill:red, stroke:#333,stroke-width:2px;
            style cm    fill:red, stroke:#333,stroke-width:2px;
